e73a8e27317136ad63cee813c452ba1d55e22f9b
Petr Filaretov <petr.filaretov@gmail.com>
2021-01-28 10:52:19 +0300

    initial commit

---
+++ src/test/java/pro/filaretov/springbootstartersimple/SpringBootStarterSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.filaretov.springbootstartersimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
